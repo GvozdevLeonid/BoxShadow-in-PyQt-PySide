3e00553af75e2ed2174847287bfc72175562f973
GvozdevLeonid <gvozdev-leonid-95@yandex.ru>
2023-04-25 20:17:28 +0200

    Added support for python3.10

---
+++ Neumorphism/python_3_10/darwin/arm64/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.1.7 (trial), 000000, 2023-04-25T18:35:10.329495
+from .pyarmor_runtime import __pyarmor__
---
+++ Neumorphism/python_3_10/windows/x86/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.1.7 (trial), 000000, 2023-04-25T18:43:36.372328
+from .pyarmor_runtime import __pyarmor__
